ed288701e1ec47842ac18ec71b5107fa63b6bd94
unknown <aronilie.code@gmail.com>
2025-03-05 20:25:00 +0100

    Updated email message

--- src/utils/funcs.ts
+++ src/utils/funcs.ts
@@ -1,19 +1,20 @@
-import { TLead } from "../database/interfaces/totalum";
+import { TLead } from '../database/interfaces/totalum';
 
 export function generateLeadEmail(lead: TLead): string {
-    return `
+  return `
       Un nuevo lead ha completado el chatbot. Aquí están los detalles:
   
-      - **Nombre:** ${lead.nombre} ${lead.apellidos || ''}
-      - **Teléfono:** ${lead.telefono}
-      - **Correo electrónico:** ${lead.email || ''}
-      - **Propiedad de interés:** ${lead.propiedad_interes}
-      - **Tipo de financiación:** ${lead.financiacion}
-      - **Uso de la vivienda:** ${lead.uso_vivienda}
-      - **Finalidad de la inversión:** ${lead.fin_inversion}
-      - **Ahorros disponibles:** ${lead.ahorros_disponibles.toLocaleString()} €
+      - Nombre: ${lead.nombre} ${lead.apellidos || ''}
+      - Teléfono: ${lead.telefono}
+      - Correo electrónico: ${lead.email || ''}
+      - Propiedad de interés: ${lead.propiedad_interes}
+      - Tipo de financiación: ${lead.financiacion}
+      ${lead.uso_vivienda ? `- Uso de la vivienda: ${lead.uso_vivienda}` : ''}
+      ${lead.fin_inversion ? `- Finalidad de la inversión: ${lead.fin_inversion}` : ''}
+      ${lead.ahorros_disponibles > 0 ? `- Ahorros disponibles: ${lead.ahorros_disponibles.toLocaleString()} €` : ''}
+
+      Para acceder al Panel de leads: https://web.totalum.app/table/leads
   
       Seguimos! 🚀
     `;
-  }
-  
+}
