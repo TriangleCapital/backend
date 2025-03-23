11e8c0913bdff0056fc7bfd747b4c22199077427
unknown <aronilie.code@gmail.com>
2025-03-23 17:03:48 +0100

    Fixed royalties comparison

--- src/server/helpers/royalties.ts
+++ src/server/helpers/royalties.ts
@@ -1,10 +1,12 @@
 import { Royalty } from '../../database/interfaces';
 
 export function existsRoyalty(okupaRoyalties: Royalty[], newRoyalty: Royalty): boolean {
-  return okupaRoyalties.some(
-    (royalty) =>
-      royalty.direccion_completa === newRoyalty.direccion_completa || royalty.ref_catastral === newRoyalty.ref_catastral
-  );
+  const formattedNewDireccion = newRoyalty.direccion_completa.trim().toLowerCase();
+
+  return okupaRoyalties.some((royalty) => {
+    const formattedDireccion = royalty.direccion_completa.trim().toLowerCase();
+    return formattedDireccion === formattedNewDireccion || royalty.ref_catastral === newRoyalty.ref_catastral;
+  });
 }
 
 export function filterValidRoyalties(royalties: Royalty[]): Royalty[] {
